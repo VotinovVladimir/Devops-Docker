70dc7f0f4b9d1838f1d1cb0d2765a4e2fc660e19
Your Name <you@example.com>
2025-10-19 20:18:22 +0300

    add project

---
+++ 06/hello.c
@@ -0,0 +1,19 @@
+#include <fcgiapp.h>
+
+int main(void) {
+    FCGX_Init();
+    int sockfd = FCGX_OpenSocket(":8080", 100);
+    if (sockfd < 0) return 1; 
+
+    FCGX_Request req;
+    FCGX_InitRequest(&req, sockfd, 0);
+    
+
+    while (FCGX_Accept_r(&req) >= 0) {
+        FCGX_FPrintF(req.out, "Content-Type: text/html\n\n");
+        FCGX_FPrintF(req.out, "hello world");
+        FCGX_Finish_r(&req);
+    }
+    
+    return 0;
+}
